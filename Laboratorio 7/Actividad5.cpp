38166808c1e0f7773df1384e5ea9b9a637df2b22
Romi32 <83616056+Romi32@users.noreply.github.com>
2021-06-07 20:05:09 -0400

    Add files via upload

---
+++ Laboratorio 7/Actividad5.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int suma;
+int num;
+int main(){ 
+printf("Ingrese un número:");
+scanf("%d",&num);
+do{
+  suma=suma+num;
+printf("Total= %d\n", suma);
+scanf("%d",&num);
+}
+while(num>=0);
+printf("Fin\n");
+return 0;
+}
